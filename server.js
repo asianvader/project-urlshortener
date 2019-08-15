397d0a45d6f390030956b7a4e03ecc1853dc5e5e
asianvader <phoebe.voong@gmail.com>
2019-08-16 00:14:56 +0100

    create unique short_url code

--- server.js
+++ server.js
@@ -44,41 +44,73 @@ app.get('/', function(req, res){
 
 });
 
+
+
 // POST => get url from form
 app.post("/api/shorturl/new", (req, res) => {
-  let original_url = req.body.url;
+  let originalUrl = req.body.url;
 
   // check to see if url starts with http(s)://www.
-  if (/^https?:\/\/www./.test(original_url)) {
+  if (/^https?:\/\/www./.test(originalUrl)) {
 
     // if true check to see if url is a valid domain
-    // let dnsUrlCheck = original_url.replace(/^https?:\/\//, "");
-    let dnsUrlCheck = new URL(original_url);
-    console.log(dnsUrlCheck.hostname);
-  
+    let dnsUrlCheck = new URL(originalUrl);
+    // console.log(dnsUrlCheck.hostname);
+
     dns.lookup(dnsUrlCheck.hostname, (err, value) => {
-      // Not a valid domain:
+      // If not a valid domain:
       if (err) {
         res.json({error: "invalid URL"});
         console.log(err);
         return;
       }
-      // a valid domain
-      console.log(value);
-      let longUrl = new Url({
-        original_url: original_url,
-        short_url: 5
-      });
-      longUrl
-        .save()
-        .then(result => {  
-          console.log('added to mongo'); 
-        })
-        .catch(err => {
-          console.log(err);
-        }); 
-      res.json({original_url: original_url});
+
+      // if a valid domain
+
+      // check MongoDB to see if doc exists
+      let checkIfUrlExists = Url.findOne({'original_url': originalUrl}, (err, data) => {
+        // if doc exists in db
+        if (data) {
+          console.log(data);
+          res.json({
+            document: "exists",
+            original_url: data.original_url,
+            short_url: data.short_url
+          });
+          return;
+        }
+        
+        // if doc DOESN'T exist in db
+        // count total documents. Increment by 1 to create short_url
+        let count = 0;
+        let totalUrls = Url.countDocuments({'short_url': {$gt:0}}, (err, data) => {
+          count = data + 1;
+          
+          // create new document in MongoDB
+          let longUrl = new Url({
+            original_url: originalUrl,
+            short_url: count
+          });
+          longUrl
+            .save()
+            .then(result => {  
+              res.json({
+                original_url: originalUrl,
+                short_url: count
+              });
+            })
+            .catch(err => {
+              console.log("Add URL to MongoDB " + err);
+            }); 
+          }).catch(err => {
+          console.log("totalUrls " + err);
+          });
+        }).catch(err => {
+          console.log("Check if URL exists " + err);
+        });
+
     })
+    // invalid URL input in form
   } else {
     res.json({error: "invalid URL"});
   }
